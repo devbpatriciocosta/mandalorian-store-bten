e7f2d5b759677d114bf51cff609994a726625c18
Bruno Patrício <101806366+devbpatriciocosta@users.noreply.github.com>
2023-10-22 15:21:51 -0300

    Changes in package and server to deploy

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoute from './routes/authRoute.js';
 import productsCategoriesRoutes from './routes/productsCategoriesRoute.js';
 import productsDetailsRoute from './routes/productsDetailsRoute.js';
 import cors from 'cors';
+import path from 'parth';
 
 
 //Env Configuration
@@ -22,17 +23,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, './client/build')))
+
 
 //Routes Configuration
 app.use("/api/v1/auth", authRoute);
 app.use("/api/v1/category", productsCategoriesRoutes);
 app.use("/api/v1/product", productsDetailsRoute);
 
+
 //REST API
-app.get('/', (req, res) => {
-    res.send(
-        "<h1>Welcome to THE Mandalorian Store!</h1>"
-    )
+app.use('*', function(req, res) {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 //PORT server configuration
